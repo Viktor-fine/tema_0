b0e97758a95d171d69be67c8a230179458658c16
Viktor-fine <vik-mailll@mail.ru>
2023-10-10 16:19:04 +0300

    Заливка проекта на Django доска объявлений

---
+++ standart/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
+
+from .models import Bb, Rubric
+from .forms import BbForm
+
+def index(request): 
+    bbs = Bb.objects.all()
+    rubrics = Rubric.objects.all()
+    context = {'bbs': bbs, 'rubrics': rubrics}
+    return render(request, 'blank/index.html', context)
+
+def rubric_bbs(request, rubric_id):
+    bbs = Bb.objects.filter(rubric=rubric_id)
+    rubrics = Rubric.objects.all()
+    current_rubric = Rubric.objects.get(pk=rubric_id)
+    context = {'bbs': bbs, 'rubrics': rubrics, 'current_rubric': current_rubric}
+    return render(request, 'blank/rubric_bbs.html', context)
+
+
+class BbCreateView(CreateView):
+    template_name = 'blank/bb_create.html'
+    form_class = BbForm
+    success_url = reverse_lazy('index')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['rubrics'] = Rubric.objects.all()
+        return context
